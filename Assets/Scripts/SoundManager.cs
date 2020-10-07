cac8412bca30d56a1825624c136e6e12646c3412
Brigitte Demers <guyoreo@gmail.com>
2020-10-07 10:34:11 -0400

    Chapter Seven: Page 191 Completed-Added Sounds

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    // Instance stores a static reference to the single instance of 
+    // SoundManager.
+    public static SoundManager Instance = null;
+
+    // These variables represent the clips for each sound effect in the game.
+    public AudioClip gunFire;
+    public AudioClip upgradedGunFire;
+    public AudioClip hurt;
+    public AudioClip alienDeath;
+    public AudioClip marineDeath;
+    public AudioClip Victory;
+    public AudioClip elevatorArrived;
+    public AudioClip powerUpPickUp;
+    public AudioClip powerUpAppear;
+
+    // Refers to the audio source added to the SoundManager.
+    private AudioSource soundEffectAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Singleton pattern. Ensures there is always one copy of this 
+        // object in existence. Destroys itself if there is more then one
+        // in existence.
+        if (Instance == null)
+        {
+            Instance = this;
+        } else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        // Searches for the correct Audio Source.
+        UnityEngine.AudioSource[] sources = GetComponents<AudioSource>();
+        foreach (AudioSource source in sources)
+        {
+            if (source.clip == null)
+            {
+                soundEffectAudio = source;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
